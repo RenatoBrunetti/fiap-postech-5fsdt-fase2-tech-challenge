711143fca0744a7791717e31a29434933bc1b5da
Renato Brunetti <recarapia@gmail.com>
2025-07-24 15:22:11 -0300

    update init sql file

--- scripts/db/init.sql
+++ scripts/db/init.sql
@@ -1,6 +1,6 @@
-create table "role" (
-  "id" uuid primary key,
-  "name" varchar(255) not null,
+create table if not exists "role" (
+  "id" uuid primary key default gen_random_uuid(),
+  "name" varchar(255) unique not null,
   "description" varchar(255),
   "created_at" timestamp not null default current_timestamp,
   "updated_at" timestamp not null default current_timestamp
@@ -10,34 +10,34 @@ insert into "role" ("id", "name", "description") values
   (gen_random_uuid(), 'admin', 'Administrator with full access'),
   (gen_random_uuid(), 'student', 'Student with limited access');
 
-create table "user" (
-	"id" uuid primary key,
+create table if not exists "user" (
+	"id" uuid primary key default gen_random_uuid(),
 	"username" varchar(255) not null,
 	"password" varchar(255) not null,
   "active" boolean not null default true,
-	"created_at" timestamp not null default current_timestamp,
-	"updated_at" timestamp not null default current_timestamp,
+	"created_at" timestamp without time zone default current_timestamp,
+	"updated_at" timestamp without time zone default current_timestamp,
 
-  "role_id" uuid not null references "role"("id") on delete cascade
+  "role_id" uuid not null references "role"("id") on delete restrict -- Impede a exclusão de uma role se houver usuários associados
 );
 
-create table "post" (
-  "id" uuid primary key,
+create table if not exists "post" (
+  "id" uuid primary key default gen_random_uuid(),
   "title" varchar(255) not null,
   "content" text not null,
   "active" boolean not null default true,
-  "created_at" timestamp not null default current_timestamp,
-  "updated_at" timestamp not null default current_timestamp,
+  "created_at" timestamp without time zone default current_timestamp,
+  "updated_at" timestamp without time zone default current_timestamp,
 
-  "user_id" uuid not null references "user"("id") on delete cascade
+  "user_id" uuid not null references "user"("id") on delete cascade -- Se um usuário for deletado, suas postagens também serão deletadas
 );
 
-create table "post_log" (
-  "id" uuid primary key,
-  "action" varchar(255) not null,
-  "created_at" timestamp not null default current_timestamp,
-  "updated_at" timestamp not null default current_timestamp,
+create table if not exists "post_log" (
+  "id" uuid primary key default gen_random_uuid(),
+  "action" varchar(255) not null, -- Ex: 'created', 'updated', 'deleted'
+  "created_at" timestamp without time zone default current_timestamp,
+  "updated_at" timestamp without time zone default current_timestamp,
 
-  "post_id" uuid not null references "post"("id") on delete cascade,
-  "user_id" uuid not null references "user"("id") on delete cascade
+  "post_id" uuid not null references "post"("id") on delete cascade, -- Se uma postagem for deletada, seus logs também serão deletados
+  "user_id" uuid not null references "user"("id") on delete set null -- Se o usuário que realizou a ação for deletado, o user_id no log será NULL
 );
