696eff15e6de57c1a56d104126e9f30f0a0708be
Renato Brunetti <recarapia@gmail.com>
2025-07-30 10:11:51 -0300

    feat: add post logs on update and delete

--- src/http/controllers/post/delete.ts
+++ src/http/controllers/post/delete.ts
@@ -2,6 +2,8 @@ import { FastifyReply, FastifyRequest } from 'fastify';
 import { z } from 'zod';
 
 import { MakeDeletePostUseCase } from '@/use-cases/factories/make-delete-post';
+import { MakeFindPostUseCase } from '@/use-cases/factories/make-find-post';
+import { makeCreatePostLogUseCase } from '@/use-cases/factories/make-create-post-log';
 
 export async function deletePost(request: FastifyRequest, reply: FastifyReply) {
   const registerParamsSchema = z.object({
@@ -9,8 +11,21 @@ export async function deletePost(request: FastifyRequest, reply: FastifyReply) {
   });
 
   const { id } = registerParamsSchema.parse(request.params);
+  const findPostUseCase = MakeFindPostUseCase();
+  const post = await findPostUseCase.handler(id);
+
+  // Create a PostLog instance
+  if (post && post.id) {
+    const createPostLogUseCase = makeCreatePostLogUseCase();
+    await createPostLogUseCase.handler({
+      action: 'delete',
+      post_id: post.id,
+      user_id: post.user_id,
+    });
+  }
 
   const deletePostUseCase = MakeDeletePostUseCase();
-  const post = await deletePostUseCase.handler(id);
+  await deletePostUseCase.handler(id);
+
   return reply.status(200).send({ message: 'Post deleted successfully' });
 }
--- src/http/controllers/post/update.ts
+++ src/http/controllers/post/update.ts
@@ -2,6 +2,7 @@ import { FastifyReply, FastifyRequest } from 'fastify';
 import { z } from 'zod';
 
 import { MakeUpdatePostUseCase } from '@/use-cases/factories/make-update-post';
+import { makeCreatePostLogUseCase } from '@/use-cases/factories/make-create-post-log';
 
 export async function updatePost(request: FastifyRequest, reply: FastifyReply) {
   const registerParamsSchema = z.object({
@@ -11,13 +12,24 @@ export async function updatePost(request: FastifyRequest, reply: FastifyReply) {
   const { id } = registerParamsSchema.parse(request.params);
 
   const registerBodySchema = z.object({
-    title: z.string(),
-    content: z.string(),
+    title: z.string().optional(),
+    content: z.string().optional(),
     user_id: z.string(),
   });
 
   const { title, content, user_id } = registerBodySchema.parse(request.body);
   const updatePostUseCase = MakeUpdatePostUseCase();
   const post = await updatePostUseCase.handler({ title, content, user_id, id });
+
+  // Create a PostLog instance
+  if (post && post.id) {
+    const createPostLogUseCase = makeCreatePostLogUseCase();
+    await createPostLogUseCase.handler({
+      action: 'update',
+      post_id: post.id,
+      user_id,
+    });
+  }
+
   return reply.status(200).send(post);
 }
